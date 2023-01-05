b9a48a09227d8d139fc95b1aec76e4dd35fe7484
Hyuk Park <hyukpk@hotmail.com>
2023-01-04 17:12:36 -0800

    Package name changed to "org.bcit.comp2522.lab01". "build.gradle" file adjusted to change in package name. Print statement in main.java file personalized and changed.

---
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -0,0 +1,7 @@
+package org.bcit.comp2522.labs.lab01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, my name is Hyuk. It is pronounced \"Hee-yuk\" (one syllable), but when I was younger I preferred other people to just pronounce it as \"Hawk\" (I thought it sounded better).");
+    }
+}
